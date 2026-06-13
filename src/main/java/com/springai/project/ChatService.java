1b2a495c4e2eb23c5e1df3f84c4390a89d9e286f
Vaishnavi Gupta <150041663+VaishnaviGupta2320@users.noreply.github.com>
2026-06-13 14:30:36 +0530

    Add recipe generator feature

--- src/main/java/com/springai/project/ChatService.java
+++ src/main/java/com/springai/project/ChatService.java
@@ -1,6 +1,9 @@
 package com.springai.project;
 
 import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.google.genai.GoogleGenAiChatOptions;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,4 +19,20 @@ public class ChatService {
     {
         return chatModel.call(prompt);
     }
-}
+
+    public String getResponseOptions(String prompt) {
+
+        ChatResponse response= chatModel.call(
+                new Prompt(
+                        prompt,
+                        GoogleGenAiChatOptions.builder()
+                                .temperature(0.4)
+                                .build()
+                )
+        );
+        return response.getResult().getOutput().getText();
+
+
+    }
+    }
+
--- src/main/java/com/springai/project/genAIController.java
+++ src/main/java/com/springai/project/genAIController.java
@@ -4,14 +4,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+
 @RestController
 public class genAIController {
 
-    ChatService chatService;
+    private final ChatService chatService;
+    private final RecipeService recipeService;
 
-    public genAIController(ChatService chatService)
+    public genAIController(ChatService chatService, RecipeService recipeService)
     {
         this.chatService=chatService;
+        this.recipeService = recipeService;
     }
 
     @GetMapping("ask-ai")
@@ -19,4 +23,19 @@ public class genAIController {
     {
         return chatService.getResponse(prompt);
     }
+
+
+@GetMapping("ask-ai-options")
+public String getResponseOptions(@RequestParam String prompt)
+{
+    return chatService.getResponseOptions(prompt);
+}
+
+@GetMapping("recipe-creator")
+    public String recipeCreator(@RequestParam String ingredients,
+                                      @RequestParam(defaultValue = "any") String cuisine,
+                                      @RequestParam(defaultValue = "") String dietaryRestrictions) {
+        return recipeService.createRecipe(ingredients, cuisine, dietaryRestrictions);
+}
+
 }
